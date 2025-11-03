c06b811b43e5021bbbbc75cf6c5c66941dc71066
Cornelius Motanya <motanyac@gmail.com>
2025-11-03 19:32:57 +0300

    refactor: remove commented-out API handler imports and exports

--- app/api/verify/route.ts
+++
@@ -1,7 +0,0 @@
-// import { DELETEHandler } from "./delete";
-// import { GETHandler } from "./get";
-// import { POSTHandler } from "./post";
-
-// export const GET = GETHandler;
-// export const POST = POSTHandler;
-// export const DELETE = DELETEHandler;
